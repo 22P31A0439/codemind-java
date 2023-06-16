5a34982abeadc0d3a216a7c5246cf36367727d78
22P31A0439 <120160275+22P31A0439@users.noreply.github.com>
2023-06-16 19:42:12 +0530

    import java.util.*; public class solution {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         char ch;         ch=sc.next().charAt(0);         int ascii = ch;         int castAscii = (int) ch;         System.out.println(castAscii);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class solution
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        char ch;
+        ch=sc.next().charAt(0);
+        int ascii = ch;
+        int castAscii = (int) ch;
+        System.out.println(castAscii);
+    }
+}
